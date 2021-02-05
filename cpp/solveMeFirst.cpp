a0fcadcee1685e791ff88509219eb7b859bf3833
gowshiselva <gowshigan.selvarasa@aalto.fi>
2021-02-05 23:34:15 +0200

    solve me first

---
+++ cpp/solveMeFirst.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  if ((a>0 && a<=1000) && (b>0 && b<=1000))
+  {
+      return (a+b);
+  }
+  else {
+  return 0;
+  }
+  
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
+
